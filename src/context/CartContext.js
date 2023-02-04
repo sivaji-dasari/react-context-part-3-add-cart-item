f64cb6222b2f004854f5d486735dc65c70b8eec0
sivaji <shivajidaasari@gmail.com>
2023-02-04 11:38:46 +0000

    react context part 3 add cart item

---
+++ src/context/CartContext.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const CartContext = React.createContext({
+  cartList: [
+    {
+      title: 'Product 1',
+      brand: 'Brand Name',
+      id: 1001,
+      imageUrl: 'https://assets.ccbp.in/frontend/react-js/sample-product-img',
+      price: 760,
+      Quantity: 5,
+    },
+    {
+      title: 'Product 2',
+      brand: 'Brand Name',
+      id: 1002,
+      imageUrl: 'https://assets.ccbp.in/frontend/react-js/sample-product-img',
+      price: 760,
+      Quantity: 2,
+    },
+  ],
+  addCartItem: () => {},
+  deleteCartItem: () => {},
+})
+
+export default CartContext
